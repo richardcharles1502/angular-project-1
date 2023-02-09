f2f742c1f45eedd1bbcd9c67af598dd2f490b2a8
Richard Charles <richardcharles.p@prodapt.com>
2023-02-09 12:17:11 +0530

    two file commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestCompComponent } from './test-comp/test-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestCompComponent
   ],
   imports: [
     BrowserModule,
